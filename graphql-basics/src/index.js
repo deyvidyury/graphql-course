54d9a809e72f1387fb5abbb5acdc7d5cc1d8a32e
Deyvid Lopes <deyvidyury@yahoo.com.br>
2019-04-06 08:24:04 -0300

    import and exports

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,10 @@
-console.log("hello");
+import myCurrentLocation, {
+    message,
+    name,
+    getGreeting
+} from "./myModule";
+
+console.log(message);
+console.log(name);
+console.log(myCurrentLocation);
+console.log(getGreeting('Jessica'));
